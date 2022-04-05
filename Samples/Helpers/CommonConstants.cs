9a54e84c4c1769ac80ad2a4825017a554cd5d3fa
Thomas Galliker <thomas@bluewin.ch>
2022-04-05 22:06:29 +0200

    Add appcenter api keys

--- Samples/Helpers/CommonConstants.cs
+++ Samples/Helpers/CommonConstants.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Samples.Helpers
+﻿namespace Samples.Helpers
 {
     public static class CommonConstants
     {
-        internal const string AppCenterAndroid = "AC_ANDROID";
-        internal const string AppCenteriOS = "AC_IOS";
+        internal const string AppCenterAndroid = "c45fb280-e432-44cb-8b1c-eefa1c63fbd4";
+        internal const string AppCenteriOS = "ad52a6d1-2e81-4af1-9398-54e53603311e";
         internal const string AppCenterUWP = "AC_UWP";
     }
 }
