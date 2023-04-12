6b4ecb3a45d666899514f46757ed5aa37aeb8ae8
Thomas Galliker <thomas@bluewin.ch>
2023-04-12 22:33:18 +0200

    Add error/crash tracking

--- Samples/View/HomePage.xaml.cs
+++ Samples/View/HomePage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Samples.Model;
 using Xamarin.Forms;
 
@@ -20,7 +22,7 @@ namespace Samples.View
                 return;
             }
 
-            // DEMO: Use TrackEvent to write diagnostics info to App Center
+            // Demo: Use TrackEvent to write diagnostics info to AppCenter
             var properties = new Dictionary<string, string>
             {
                 { "PageName", $"{item.PageType.Name}"}
@@ -28,7 +30,23 @@ namespace Samples.View
             Analytics.TrackEvent("PageView", properties);
             Analytics.TrackEvent($"PageView: {item.PageType.Name}");
 
-            await this.Navigation.PushAsync((Page)Activator.CreateInstance(item.PageType));
+            try
+            {
+                await this.Navigation.PushAsync((Page)Activator.CreateInstance(item.PageType));
+
+                if (item.PageType == typeof(BatteryPage))
+                {
+                    throw new InvalidOperationException("This is just a test exception", new NullReferenceException("Something cannot be null"));
+                }
+            }
+            catch (Exception ex)
+            {
+                // Demo: Use TrackError to write error/crash information to AppCenter
+                Crashes.TrackError(ex);
+            }
+
+            // Demo: App crashes are automatically reported to AppCenter
+            //throw new Exception("Test");
 
             // deselect Item
             ((ListView)sender).SelectedItem = null;
